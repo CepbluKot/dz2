192c3bda23a74f52c55dc806b6052b394217b339
me <igorcat100@gmail.com>
2020-10-25 21:19:22 +0300

    добавлены математические функции
    add sum
    
    add mult
    
    add subt
    
    add divi
    
    add test

--- main.c
+++ main.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
-2
- 
-3
-    int main() {
-4
-      printf("hello world");
-5
-      return 0;
-6
-    }
+
+int sum(int a, int b){
+    return a + b;
+}
+
+int mult(int a, int b){
+    return a * b;
+}
+
+int subt(int a, int b){
+    return a - b;
+}
+
+int divi(int a, int b){
+    return a / b;
+}
+
+int test(){
+    printf("2 + 3 = %d", sum(2,3));
+    printf("\n5 * 10 = %d", mult(5,10));
+    printf("\n9 - 3 = %d", subt(9,3));
+    printf("\n14 / 7 = %d", divi(14,7));
+    
+}
+
+int main()
+{
+    print("hello world");
+
+    return 0;
+}
