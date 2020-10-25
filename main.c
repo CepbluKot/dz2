478cbad754f3d98a6ffb102f1ebf2db511887336
me <igorcat100@gmail.com>
2020-10-25 21:32:36 +0300

    add test

--- main.c
+++ main.c
@@ -16,11 +16,17 @@ int divi(int a, int b){
     return a / b;
 }
 
-
+int test(){
+    printf("2 + 3 = %d", sum(2,3));
+    printf("\n5 * 10 = %d", mult(5,10));
+    printf("\n9 - 3 = %d", subt(9,3));
+    printf("\n14 / 7 = %d", divi(14,7));
+    
+}
 
 int main()
 {
-    printf("hello world");
+    print("hello world");
 
     return 0;
 }
